e2c73cb37ab2b69b407b626697a2ab9ffae0525d
Jacob <beefycapo@gmail.com>
2025-09-13 23:27:44 -0400

    Added Fermi run state, split parameters for speed and acceleration between states

---
+++ scripts/entities/fermi_run.gd
@@ -0,0 +1,17 @@
+extends FermiState
+class_name FermiRun
+
+
+func _exit_state() -> void:
+	pass
+
+func _process_state(delta:float) -> void:
+	p_parent.rotate_from_input(delta)
+	p_parent.run_from_input(delta)
+	
+	if p_parent.input.is_jumping():
+		p_parent.set_state("jump")
+	
+	
+	if p_parent.input.get_movement_vector() == Vector3.ZERO:
+		p_parent.set_state("walk")
